13c555210a73f95c57ed7da79b16ea19cc6a6ee6
Yugalkumar619 <71225190+Yugalkumar619@users.noreply.github.com>
2021-10-20 13:16:10 +0530

    Basic Paging and error solved using GONE in progress bar

--- app/src/main/java/com/yugal/newsapiclient/NewsFragment.kt
+++ app/src/main/java/com/yugal/newsapiclient/NewsFragment.kt
@@ -5,8 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AbsListView
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.yugal.newsapiclient.databinding.FragmentNewsBinding
 import com.yugal.newsapiclient.presentation.adapter.NewsAdapter
 import com.yugal.newsapiclient.presentation.viewmodel.NewsViewModel
@@ -18,6 +20,10 @@ class NewsFragment : Fragment() {
     private lateinit var fragmentNewsBinding: FragmentNewsBinding
     private var country = "us"
     private var page = 1
+    private var isScrolling = false
+    private var isLoading = false
+    private var isLastPage = false
+    private var pages = 0
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -44,6 +50,12 @@ class NewsFragment : Fragment() {
                     response.data?.let {
                         newsAdapter.differ.submitList(it.articles.toList())
                         val message = it.articles.toList()
+                        if(it.totalResults%20 == 0) {
+                            val pages = it.totalResults / 20
+                        }else{
+                            pages = it.totalResults/20+1
+                        }
+                        isLastPage = page == pages
                     }
                 }
                 is com.yugal.newsapiclient.data.util.Resource.Error->{
@@ -64,16 +76,44 @@ class NewsFragment : Fragment() {
         fragmentNewsBinding.rvNews.apply {
             adapter = newsAdapter
             layoutManager = LinearLayoutManager(activity)
+            addOnScrollListener(this@NewsFragment.onScrollListener)
         }
 
     }
 
 
     private fun showProgressBar(){
+        isLoading = true
         fragmentNewsBinding.progressBars.visibility = View.VISIBLE
     }
 
     private fun hideProgressBar(){
-        fragmentNewsBinding.progressBars.visibility = View.INVISIBLE
+        isLoading = false
+        fragmentNewsBinding.progressBars.visibility = View.GONE
+    }
+
+    private val onScrollListener = object : RecyclerView.OnScrollListener(){
+        override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+            if(newState == AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL){
+                isScrolling = true
+            }
+        }
+
+        override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+            super.onScrolled(recyclerView, dx, dy)
+            val layoutManager = fragmentNewsBinding.rvNews.layoutManager as LinearLayoutManager
+            val sizeOfTheCurrentList = layoutManager.itemCount
+            val visibleItems = layoutManager.childCount
+            val topPosition = layoutManager.findFirstVisibleItemPosition()
+
+            val hasReachedToEnd = topPosition + visibleItems >= sizeOfTheCurrentList
+            val shouldPaginate = !isLoading && !isLastPage && hasReachedToEnd && isScrolling
+            if(shouldPaginate){
+                page++
+                viewModel.getNewsHeadLines(country,page)
+                isScrolling = false
+            }
+
+        }
     }
 }
